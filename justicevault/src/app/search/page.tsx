c972e657857e8da66a074b9d888f3ca3c41f5e41
ansonantony <ansonantony89@gmail.com>
2024-03-19 23:58:38 +0530

    Pages Added

--- justicevault/src/app/search/page.tsx
+++ justicevault/src/app/search/page.tsx
@@ -1,13 +1,20 @@
 import Footer from "../components/Footer";
-import Header from "../components/Header";
+import NavBar from "../components/NavBar";
 
 export default function Search() {
     return (
       <>
         <div className="root">
-        <Header  btnval="Log Out" dival ="Get Case Details"/>
-
-       
+        <NavBar btnval='Log Out'/>
+        <form>
+        <div className="search">
+         
+            <input type="text" className="inner-shadow caseno" placeholder="Enter Case no" />
+             <input type="date" className="date" placeholder="Enter Year"/>
+            
+             
+       </div>
+       </form>
        <Footer/>
       </div>
       </>
