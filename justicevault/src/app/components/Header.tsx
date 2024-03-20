e7db4e932ff76c9dc1ee61b48cc1b6aed34f9775
ansonantony <ansonantony89@gmail.com>
2024-03-20 10:49:13 +0530

    Pages Added

--- justicevault/src/app/components/Header.tsx
+++ justicevault/src/app/components/Header.tsx
@@ -1,4 +1,5 @@
 
+import { useState } from "react";
 import NavBar from "./NavBar"
 
 
@@ -6,6 +7,7 @@ export default function Header(props: { btnval: any,dival: any }) {
 
     let buttonValue = props.btnval;
     let divValue = props.dival;
+  
     const astyle ={
         textDecoration: 'none',
         color:'black'
@@ -16,7 +18,7 @@ export default function Header(props: { btnval: any,dival: any }) {
     <div className="header">
         <h2>File storage and sharing for remote <br /> clients on <span className="web3">Web3</span></h2>
         <p>JusticeVault is a online digital vault on decentralized cloud IPFS that allows you storage, share,<br/> collect files privately .</p>
-        <a style={astyle}  href={buttonValue == 'Sign In' ?"/":"/search"}><div>{divValue}</div></a>
+        <a style={astyle}  href={divValue == 'Get Started' ?"/":"/search"}><div>{divValue}</div></a>
     </div>
     </>
     );
