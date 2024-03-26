d3fb2d9878a4f68eec60f50ba751d812dd565b52
Anson Antony <ansonantony89@gmail.com>
2024-03-27 00:03:14 +0530

    Upload page v1

--- justicevault/src/app/casedetails/page.tsx
+++ justicevault/src/app/casedetails/page.tsx
@@ -1,9 +1,7 @@
 'use client';
-import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Features from "../components/Features";
 import NavBar from "../components/NavBar";
-import Card from "../components/Card";
+import Cardf from "../components/Card";
 export default function Casedetails() {
   return (
     <>
@@ -11,9 +9,9 @@ export default function Casedetails() {
       <NavBar btnval="Log Out"/>
 
       <div className="grid md:grid-cols-3 items-start gap-4 w-full max-w-5xl mx-auto">
-       <Card/>
-       <Card/>
-       <Card/>
+       <Cardf/>
+       <Cardf/>
+       <Cardf/>
        </div>
        <Footer/>
       </div>
