36313c11146b5a08147ff21b085f6f5a4cde66c0
Anjana Kj <kjanjana0081@gmail.com>
2024-04-13 16:26:48 +0530

    basic structure

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,9 @@
+//const router = express.Router();
+//
+//// Import your controller functions
+//const { loginUser, registerUser } = require('../controllers/authController');
+//
+//router.post('/login', loginUser);
+//router.post('/register', registerUser);
+//
+//module.exports = router;
